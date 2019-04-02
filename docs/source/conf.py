fd60d35c1a8abc6c72041e10cf436d906d16a53f
daviddesancho <daviddesancho@gmail.com>
2019-04-02 16:38:28 +0200

    docs: working doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,8 +50,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -136,7 +136,6 @@ latex_documents = [
     (master_doc, 'MasterMSM.tex', 'MasterMSM Documentation',
      'David De Sancho', 'manual'),
 ]
-
 
 # -- Options for manual page output ------------------------------------------
 
