8f4cd739fb7edfec407958438d3abde5e9dcbbe1
daviddesancho <daviddesancho@gmail.com>
2019-04-04 15:21:59 +0200

    docs: fixes errors in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 #sys.path.append(os.path.join(os.path.abspath(os.pardir)))
-sys.path.append("../mastermsm.msm")
+#sys.path.append("../mastermsm.msm")
 
 
 
