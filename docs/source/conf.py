cd8c48fa599f4eee037ca7f1ab6562a070726c3e
daviddesancho <daviddesancho@gmail.com>
2019-04-04 16:24:29 +0200

    docs: tries to make sphinx compile

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 import mastermsm
 #sys.path.append(os.path.join(os.path.abspath(os.pardir)))
 
