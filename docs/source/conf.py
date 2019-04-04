d64e84aabc1c87adf3d378c07abef866400f0eae
daviddesancho <daviddesancho@gmail.com>
2019-04-04 15:14:46 +0200

    docs: changes behaviour of documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,9 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('../../mastermsm'))
-sys.path.append(os.path.join(os.path.abspath(os.pardir)))
+sys.path.insert(0, os.path.abspath('../'))
+#sys.path.append(os.path.join(os.path.abspath(os.pardir)))
+sys.path.append("../mastermsm.msm")
 
 
 
