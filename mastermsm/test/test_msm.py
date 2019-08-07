e3d6c57f37e860e6f7949c3150ffe0260ca77136
Anne A <aaguirre.rdit@gmail.com>
2019-08-07 13:26:27 +0900

    further testing of utility functions in msm_lib

--- mastermsm/test/test_msm.py
+++ mastermsm/test/test_msm.py
@@ -3,7 +3,7 @@ import mdtraj as md
 import numpy as np
 from mastermsm.trajectory import traj_lib, traj
 from mastermsm.msm import msm, msm_lib
-import os
+import os, pickle
 
 # thermal energy (kJ/mol)
 beta = 1./(8.314e-3*300)
@@ -107,6 +107,45 @@ class TestMSMLib(unittest.TestCase):
                 else:
                     self.assertAlmostEqual(d_peq_elem[i] / (test_peq[i] * (1. - test_peq[i])), -beta)
 
+    def test_partial_pfold(self):
+        states = range(3)
+        K = np.random.rand(2, 2)
+        d_K = np.random.rand(2, 2)
+        FF = [0]
+        UU = [2]
+        res_dpfold = msm_lib.partial_pfold(states, K, d_K, FF, UU,
+                                           np.random.randint(0, 2))  # the last int parameter is not used
+        self.assertEqual(len(res_dpfold), len(states))
+        self.assertIsInstance(res_dpfold, np.ndarray)
+        self.assertIsInstance(res_dpfold[0], float)
+
+    def test_partial_flux(self):
+        nstates = np.random.randint(2,50)
+        states = range(nstates)
+        peq = np.random.rand(nstates)
+        K = np.random.rand(nstates,nstates)
+        pfold = np.random.rand(nstates)
+        d_peq = np.random.rand(nstates)
+        d_K = np.random.rand(nstates,nstates)
+        d_pfold = np.random.rand(nstates)
+        target = [0]
+
+        sum_d_flux = 0
+        d_J = np.zeros((nstates, nstates), float)
+        for i in range(nstates):
+            for j in range(nstates):
+                d_J[j][i] = d_K[j][i] * peq[i] * (pfold[j] - pfold[i]) + \
+                            K[j][i] * d_peq[i] * (pfold[j] - pfold[i]) + \
+                            K[j][i] * peq[i] * (d_pfold[j] - d_pfold[i])
+                if j in target and K[j][i] > 0:  # dividing line corresponds to I to F transitions
+                    sum_d_flux += d_J[j][i]
+        res_sum_d_flux = msm_lib.partial_flux(states, peq, K, pfold,d_peq, d_K, d_pfold, target)
+
+        self.assertIsNotNone(res_sum_d_flux)
+        self.assertIsInstance(res_sum_d_flux, float)
+
+
+
     def test_tau_averages(self):
         tau_boots_test = np.random.rand(2, 2)
         keys_test = range(3)
@@ -149,6 +188,103 @@ class TestMSMLib(unittest.TestCase):
         self.assertIsInstance(res_popul, list)
         self.assertIsInstance(res_popul[0], np.ndarray)
         self.assertIsInstance(res_popul[0][0], float)
+
+    def test_detailed_balance(self):
+        nkeep_test = 2
+        rate = np.array(np.random.rand(nkeep_test,nkeep_test))
+        peq = np.random.rand(nkeep_test)
+        res_rate = msm_lib.detailed_balance(nkeep_test, rate, peq)
+        self.assertEqual(res_rate.shape, (nkeep_test,nkeep_test))
+        self.assertIsInstance(res_rate,np.ndarray)
+        self.assertIsInstance(res_rate[0][0],float)
+
+    def test_likelihood(self):
+        nkeep_test = 2
+        rate = np.array(np.random.rand(nkeep_test,nkeep_test))
+        count = np.array(np.random.randint(0, 10**5, size=(nkeep_test,nkeep_test)))
+        lagt = np.random.randint(1,1000)
+        res_mlog_like = msm_lib.likelihood(nkeep_test,rate,count,lagt)
+        self.assertIsInstance(res_mlog_like, float)
+        self.assertIsNotNone(res_mlog_like)
+        self.assertGreater(res_mlog_like, 0)
+
+    def test_calc_mlrate(self):
+        nkeep_test = 2
+        rate_init = np.array(np.random.rand(nkeep_test, nkeep_test))
+        count = np.array(np.random.randint(0, 10 ** 5, size=(nkeep_test, nkeep_test)))
+        lagt = np.random.randint(1, 1000)
+        res_rate, res_ml, res_beta = msm_lib.calc_mlrate(nkeep_test,  count, lagt, rate_init)
+        self.assertIsInstance(res_rate, np.ndarray)
+        self.assertIsNotNone(res_rate)
+        self.assertIsNotNone(res_ml)
+        self.assertIsNotNone(res_beta)
+
+    def test_mc_move(self):
+        nkeep_test = np.random.randint(2,100)
+        rate = np.random.rand(nkeep_test,nkeep_test)
+        peq_test = np.random.rand(nkeep_test)
+        db_rate = msm_lib.detailed_balance(nkeep_test,rate,peq_test)
+        new_rate, new_peq = msm_lib.mc_move(nkeep_test, db_rate, peq_test)
+        self.assertFalse(np.array_equal(db_rate, new_rate))
+        self.assertEqual(db_rate.shape, new_rate.shape)
+        self.assertEqual(peq_test.shape, new_peq.shape)
+
+    def test_calc_eigsK(self):
+        nstates = np.random.randint(2,100)
+        rate_test = np.random.rand(nstates,nstates)
+        res_tauK,res_peqK = msm_lib.calc_eigsK(rate_test)
+        self.assertIsInstance(res_tauK, list)
+
+        self.assertEqual(len(res_tauK), nstates)
+        self.assertEqual(len(res_peqK), nstates)
+        self.assertIsInstance(res_tauK[0], np.float)
+        self.assertIsInstance(res_peqK[0], np.complex)
+
+        res_tauK, res_peqK, res_rvecsK, res_lvecsK = msm_lib.calc_eigsK(rate_test, evecs=True)
+        self.assertIsNotNone(res_rvecsK)
+        self.assertIsNotNone(res_lvecsK)
+        self.assertIsInstance(res_lvecsK, np.ndarray)
+        self.assertIsInstance(res_rvecsK, np.ndarray)
+
+    def test_run_commits(self):
+        nstates = np.random.randint(2,100)
+        states = range(nstates)
+        K = np.random.rand(nstates, nstates)
+        peq = np.random.rand(nstates)
+        FF = [0]
+        UU = [2]
+        J, pfold, sum_flux, kf = msm_lib.run_commit(states, K, peq, FF, UU)
+        self.assertIsNotNone(J)
+        self.assertIsNotNone(pfold)
+        self.assertIsNotNone(sum_flux)
+        self.assertIsNotNone(kf)
+        self.assertIsInstance(kf, float)
+        self.assertGreater(kf, 0)
+        self.assertEqual(J.shape, K.shape)
+        self.assertEqual(len(pfold), nstates)
+        self.assertIsInstance(pfold[0], float)
+        self.assertIsInstance(J[0][0], float)
+
+    def test_do_boots_worker(self):
+
+        filetmp = "test_msm_temp.pickle"
+        keys = ['A', 'E']
+        lagt = np.random.randint(1,100)
+        slider = 1
+        ncount = 10
+        x = [filetmp, keys, lagt, ncount, slider]
+        # result = msm_lib.do_boots_worker(x)
+        # tauT, peqT, trans, keep_keys = result
+        # print(tauT, peqT, trans, keep_keys)
+
+
+
+
+
+
+
+
+
 
 
 
