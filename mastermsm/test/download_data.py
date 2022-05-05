ad77ef04c0981afda85cd78949a441a552afb0b7
daviddesancho <daviddesancho@gmail.com>
2022-05-05 19:12:02 +0200

    test: dowload also ala5

--- mastermsm/test/download_data.py
+++ mastermsm/test/download_data.py
@@ -10,3 +10,13 @@ def download_osf_alaTB():
     for k, v in downloads.items():
         if os.path.isfile(cpath+"/%s"%k) is False:
             urlretrieve(v, "test/data/" + k)
+
+def download_osf_ala5():
+    downloads = {"ala5.gro": "https://osf.io/6uznm/download", \
+            "ala5.xtc": "https://osf.io/gmxpy/download"}
+    cpath = os.getcwd()
+    if os.path.exists(cpath+"/test/data") is False:
+        os.mkdir(cpath+"/test/data")
+    for k, v in downloads.items():
+        if os.path.isfile(cpath+"/%s"%k) is False:
+            urlretrieve(v, "test/data/" + k)
