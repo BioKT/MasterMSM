a2312524584604b0c4ddd0354652961df964d653
daviddesancho <daviddesancho@gmail.com>
2022-05-09 16:59:50 +0200

    test: restores tests for buchete hummer

--- mastermsm/test/test_trajectory.py
+++ mastermsm/test/test_trajectory.py
@@ -8,128 +8,128 @@ import os
 class TestTimeSeries(unittest.TestCase):
     def setUp(self):
         download_osf_alaTB()
+
+        top = 'test/data/alaTB.gro'
+        xtc = 'test/data/alaTB.xtc'
+        self.traj = traj.TimeSeries(xtc=xtc, top=top)
+        self.trajs = [traj.TimeSeries(xtc=xtc, top=top) \
+                for i in range(2)]
+
+        self.dis = traj.TimeSeries(dtraj=[0,1,1,0])
+        self.dis_multi = traj.TimeSeries(dtraj=[[0,1,1,0], \
+                [1,0,0,1]])
+
+class TestMDTrajLib(unittest.TestCase):
+    def setUp(self):
+        download_osf_alaTB()
         self.ts = traj.TimeSeries(top='test/data/alaTB.gro', \
-                    trajs=['test/data/alaTB.xtc'])
+                 trajs=['test/data/alaTB.xtc'])
         self.ts_multi = traj.TimeSeries(top='test/data/alaTB.gro', \
                        trajs=['test/data/alaTB.xtc', \
                        'test/data/alaTB.xtc'])
-
-        self.dis = traj.TimeSeries(dtrajs=[0,1,1,0])
-        self.dis_multi = traj.TimeSeries(dtrajs=[[0,1,1,0], \
-                [1,0,0,1]])
-
-    def test_timeseries(self):
-        self.assertEqual(self.ts.n_trajs, 1)
-        self.assertEqual(self.ts_multi.n_trajs, 2)
-        self.assertEqual(self.dis.n_trajs, 1)
-        self.assertEqual(self.dis_multi.n_trajs, 2)
-
-#class TestMDTrajLib(unittest.TestCase):
-#    def setUp(self):
-#        download_osf_alaTB()
-#        self.ts = traj.TimeSeries(top='test/data/alaTB.gro', \
-#                 trajs=['test/data/alaTB.xtc'])
-#        self.ts_multi = traj.TimeSeries(top='test/data/alaTB.gro', \
-#                       trajs=['test/data/alaTB.xtc', \
-#                       'test/data/alaTB.xtc'])
-#        self.ts_dis = traj.TimeSeries(dtrajs=[0,1,1,0])
+        self.ts_dis = traj.TimeSeries(dtrajs=[0,1,1,0])
 
 
-#    def test_inrange(self):
-#        self.inrange = traj_lib._inrange(2, 1, 3)
-#        self.assertEqual(self.inrange, 1)
-#        self.inrange = traj_lib._inrange(0, 1, 2)
-#        self.assertEqual(self.inrange, 0)
-#        self.inrange = traj_lib._inrange(1, 1, 2)
-#        self.assertEqual(self.inrange, 0)
-#
-#    def test_inbounds(self):
-#        TBA_bounds = {}
-#        TBA_bounds['A'] = [-100., -40., -50., -10.]
-#        TBA_bounds['E'] = [-180., -40., 125., 165.]
-#        TBA_bounds['L'] = [50., 100., -40., 70.0]
-#
-#    #   test in alpha helix
-#        self.inbounds = traj_lib._inbounds(TBA_bounds['A'], -90, -40)
-#        self.assertEqual(self.inbounds, 1)
-#    #   test in beta-sheet
-#        self.inbounds = traj_lib._inbounds(TBA_bounds['E'], -90, 140)
-#        self.assertEqual(self.inbounds, 1)
-#    #   test in left-handed alpha helix
-#        self.inbounds = traj_lib._inbounds(TBA_bounds['L'], 70, 30)
-#        self.assertEqual(self.inbounds, 1)
-#    #   test when no conformation
-#        self.inbounds = traj_lib._inbounds(TBA_bounds['A'], 0, 0)
-#        self.assertEqual(self.inbounds, 0)
-#
-#
-#    def test_state(self):
-#        psi = [-30, 0, -40, 90, 140, 180]
-#        phi = [60., 0, -90, -90, -90, -180]
-#        states_test = ['L','O','A','O','E','O']
-#        bounds = {}
-#        bounds['A'] = [-100., -40., -50., -10.]
-#        bounds['E'] = [-180., -40., 125., 165.]
-#        bounds['L'] = [50., 100., -40., 70.0]
-#
-#        for ind in range(len(phi)):
-#            result = traj_lib._state(phi[ind], psi[ind], bounds)
-#            state = result[0]
-#            self.assertEqual(state, states_test[ind], 'expected state %s but got %s'%(state,states_test[ind]))
-#
-#    def test_stategrid(self):
-#        self.assertIsNotNone(traj_lib._stategrid(-180, -180, 20))
-#        self.assertLess(traj_lib._stategrid(-180, 0, 20),400)
-#        self.assertEqual(traj_lib._stategrid(0, 0, 20), 210)
-#        self.assertEqual(traj_lib._stategrid(-180, 0, 100), 2186)
-#
-#    def test_discreterama(self):
-#        mdt_test = self.tr.mdt
-#
-#        phi = md.compute_phi(mdt_test)
-#        psi = md.compute_psi(mdt_test)
-#        # print(psi)
-#        # psi = ([ 6,  8, 14, 16], [-30, 0, -40, 90, 140, 180])
-#        # phi = ([ 4,  6,  8, 14],[60., 0, -90, -90, -90, -180])
-#        states = ['L','A','E']
-#        discrete = traj_lib.discrete_rama(phi, psi, states=states)
-#        unique_st = set(discrete)
-#        for state in unique_st:
-#            self.assertIn(state, ['O', 'A', 'E', 'L'])
-#
-#    def test_discreteramagrid(self):
-#        mdt_test = self.tr.mdt
-#
-#        phi = md.compute_phi(mdt_test)
-#        psi = md.compute_psi(mdt_test)
-#        discrete = traj_lib.discrete_ramagrid(phi, psi, nbins=20)
-#        min_ibin = min(discrete)
-#        max_ibin = max(discrete)
-#        self.assertLess(max_ibin,400)
-#        self.assertGreaterEqual(min_ibin,0)
+    def test_inrange(self):
+        self.inrange = traj_lib._inrange(2, 1, 3)
+        self.assertEqual(self.inrange, 1)
+        self.inrange = traj_lib._inrange(0, 1, 2)
+        self.assertEqual(self.inrange, 0)
+        self.inrange = traj_lib._inrange(1, 1, 2)
+        self.assertEqual(self.inrange, 0)
+
+    def test_inbounds(self):
+        TBA_bounds = {}
+        TBA_bounds['A'] = [-100., -40., -50., -10.]
+        TBA_bounds['E'] = [-180., -40., 125., 165.]
+        TBA_bounds['L'] = [50., 100., -40., 70.0]
+
+    #   test in alpha helix
+        self.inbounds = traj_lib._inbounds(TBA_bounds['A'], -90, -40)
+        self.assertEqual(self.inbounds, 1)
+    #   test in beta-sheet
+        self.inbounds = traj_lib._inbounds(TBA_bounds['E'], -90, 140)
+        self.assertEqual(self.inbounds, 1)
+    #   test in left-handed alpha helix
+        self.inbounds = traj_lib._inbounds(TBA_bounds['L'], 70, 30)
+        self.assertEqual(self.inbounds, 1)
+    #   test when no conformation
+        self.inbounds = traj_lib._inbounds(TBA_bounds['A'], 0, 0)
+        self.assertEqual(self.inbounds, 0)
+
+
+    def test_state(self):
+        psi = [-30, 0, -40, 90, 140, 180]
+        phi = [60., 0, -90, -90, -90, -180]
+        states_test = ['L','O','A','O','E','O']
+        bounds = {}
+        bounds['A'] = [-100., -40., -50., -10.]
+        bounds['E'] = [-180., -40., 125., 165.]
+        bounds['L'] = [50., 100., -40., 70.0]
+
+        for ind in range(len(phi)):
+            result = traj_lib._state(phi[ind], psi[ind], bounds)
+            state = result[0]
+            self.assertEqual(state, states_test[ind], 'expected state %s but got %s'%(state,states_test[ind]))
+
+    def test_stategrid(self):
+        self.assertIsNotNone(traj_lib._stategrid(-180, -180, 20))
+        self.assertLess(traj_lib._stategrid(-180, 0, 20),400)
+        self.assertEqual(traj_lib._stategrid(0, 0, 20), 210)
+        self.assertEqual(traj_lib._stategrid(-180, 0, 100), 2186)
+
+    def test_discreterama(self):
+        mdt_test = self.tr.mdt
+
+        phi = md.compute_phi(mdt_test)
+        psi = md.compute_psi(mdt_test)
+        # print(psi)
+        # psi = ([ 6,  8, 14, 16], [-30, 0, -40, 90, 140, 180])
+        # phi = ([ 4,  6,  8, 14],[60., 0, -90, -90, -90, -180])
+        states = ['L','A','E']
+        discrete = traj_lib.discrete_rama(phi, psi, states=states)
+        unique_st = set(discrete)
+        for state in unique_st:
+            self.assertIn(state, ['O', 'A', 'E', 'L'])
+
+    def test_discreteramagrid(self):
+        mdt_test = self.tr.mdt
+
+        phi = md.compute_phi(mdt_test)
+        psi = md.compute_psi(mdt_test)
+        discrete = traj_lib.discrete_ramagrid(phi, psi, nbins=20)
+        min_ibin = min(discrete)
+        max_ibin = max(discrete)
+        self.assertLess(max_ibin,400)
+        self.assertGreaterEqual(min_ibin,0)
 
 class TestMDtraj(unittest.TestCase):
     def setUp(self):
         download_osf_alaTB()
-        self.ts = traj.TimeSeries(top='test/data/alaTB.gro', \
-                    trajs=['test/data/alaTB.xtc'])
-        self.traj = md.load('test/data/alaTB.xtc', \
-                top='test/data/alaTB.gro')
-        self.topfn = 'test/data/alaTB.gro'
-        self.trajfn = 'test/data/alaTB.xtc'
+        self.gro = 'test/data/alaTB.gro'
+        self.xtc = 'test/data/alaTB.xtc'
+        self.mdtraj = traj_lib.load_mdtraj(xtc=self.xtc, top=self.gro)
+        self.mdtrajs = [traj_lib.load_mdtraj(xtc=self.xtc, \
+                top=self.gro) \
+                for i in range(2)]
 
     def test_traj(self):
-        self.assertIsNotNone(self.traj)
-        self.assertEqual(self.traj.n_atoms, 19)
-        self.assertEqual(self.traj.timestep, 5.)
-        self.assertEqual(self.traj.n_residues, 3)
-        self.assertEqual(self.traj.n_frames, 40001)
+        self.assertIsNotNone(self.mdtraj)
+        self.assertEqual(self.mdtraj.n_atoms, 19)
+        self.assertEqual(self.mdtraj.timestep, 5.)
+        self.assertEqual(self.mdtraj.n_residues, 3)
+        self.assertEqual(self.mdtraj.n_frames, 40001)
+
+    def test_trajs(self):
+        self.assertEqual(len(self.mdtrajs), 2)
+        self.assertEqual(self.mdtrajs[0].n_atoms, 19)
+        self.assertEqual(self.mdtrajs[0].timestep, 5.)
+        self.assertEqual(self.mdtrajs[0].n_residues, 3)
 
     def test_load_mdtraj(self):
-        mdtraj = traj_lib.load_mdtraj(top=self.topfn, traj=self.trajfn)
-        self.assertIsNotNone(mdtraj)
-        self.assertEqual(mdtraj.__module__, 'mdtraj.core.trajectory')
-        self.assertEqual(hasattr(mdtraj, '__class__'), True)
+        self.assertIsNotNone(self.mdtraj)
+        self.assertEqual(self.mdtraj.__module__, 'mdtraj.core.trajectory')
+        self.assertEqual(hasattr(self.mdtraj, '__class__'), True)
 
 #    def test_read_distraj(self):
 #        self.assertIsNotNone(self.tr._read_distraj)
@@ -164,12 +164,12 @@ class TestMDtraj(unittest.TestCase):
 #        self.assertEqual(len(cstates), 0)
 #        self.assertEqual(dt, 1.0)
 #
-    def test_timeseries_init(self):
-        self.assertIsNotNone(self.ts)
-        traj = self.ts.trajs[0]
-        self.assertIsNotNone(traj.mdt)
-        self.assertEqual(hasattr(traj.mdt, '__class__'), True)
-        self.assertEqual(traj.mdt.__module__ , 'mdtraj.core.trajectory')
+#    def test_timeseries_init(self):
+#        self.assertIsNotNone(self.ts)
+#        traj = self.ts.trajs[0]
+#        self.assertIsNotNone(traj.mdt)
+#        self.assertEqual(hasattr(traj.mdt, '__class__'), True)
+#        self.assertEqual(traj.mdt.__module__ , 'mdtraj.core.trajectory')
 #        self.assertIsNotNone(self.tr.discretize)
 #
 #    def test_ts_discretize(self):
@@ -213,11 +213,14 @@ class TestMDtraj(unittest.TestCase):
 class UseMDtraj(unittest.TestCase):
     def setUp(self):
         download_osf_alaTB()
-        self.ts = traj.TimeSeries(top='test/data/alaTB.gro', \
-                trajs=['test/data/alaTB.xtc'])
+        top = 'test/data/alaTB.gro'
+        xtc = 'test/data/alaTB.xtc'
+        self.traj = traj.TimeSeries(xtc=xtc, top=top)
+        self.trajs = [traj.TimeSeries(xtc=xtc, top=top) \
+                for i in range(2)]
 
     def test_atributes(self):
-        tr = self.ts.trajs[0]
+        tr = self.traj
         self.assertIsNotNone(tr.mdt)
         self.assertEqual(tr.mdt.n_atoms, 19)
         self.assertEqual(tr.mdt.n_frames, 40001)
@@ -228,8 +231,11 @@ class UseMDtraj(unittest.TestCase):
 class TestDiscretizer(object):
     def setUp(self):
         download_osf_alaTB()
-        self.ts = traj.TimeSeries(top='test/data/alaTB.gro', \
-                trajs=['test/data/alaTB.xtc'])
+        top = 'test/data/alaTB.gro'
+        xtc = 'test/data/alaTB.xtc'
+        self.traj = traj.TimeSeries(xtc=xtc, top=top)
+        self.trajs = [traj.TimeSeries(xtc=xtc, top=top) \
+                for i in range(2)]
 
     def test_create_discretizer(self):
         disc = traj.Discretizer(self.ts)
