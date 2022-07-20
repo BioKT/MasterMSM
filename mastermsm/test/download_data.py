0c852b5ef51db31008b0bf28d3b9c3a5e3407dc4
daviddesancho <daviddesancho@gmail.com>
2022-07-20 11:33:26 +0200

    test: fixes download data

--- mastermsm/test/download_data.py
+++ mastermsm/test/download_data.py
@@ -5,8 +5,8 @@ def download_osf_alaTB():
     downloads = {"alaTB.gro": "https://osf.io/hgbqs/download", \
             "alaTB.xtc": "https://osf.io/ujmhc/download"}
     cpath = os.getcwd()
-    if os.path.exists(cpath+"/test/data") is False:
-        os.mkdir(cpath+"/test/data")
+    if os.path.exists(cpath + "/test/data") is False:
+        os.mkdir(cpath + "/test/data")
     for k, v in downloads.items():
         if os.path.isfile(cpath+"/%s"%k) is False:
             urlretrieve(v, "test/data/" + k)
